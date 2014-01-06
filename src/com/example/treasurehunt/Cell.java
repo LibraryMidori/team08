5e10749a8a30181147073b69e7f5a69f1a557c60
Cường Phạm <cuongphambk2010@gmail.com>
2014-01-06 14:50:31 +0700

    Modify class Cell
    Add some basic properties to control cells

--- src/com/example/treasurehunt/Cell.java
+++ src/com/example/treasurehunt/Cell.java
@@ -1,5 +1,16 @@
 package com.example.treasurehunt;
 
-public class Cell {
+import android.content.Context;
+import android.widget.Button;
+
+public class Cell extends Button {
+	private boolean isTraped; 			// The cell is trap or not
+	private boolean isTreasure;			// The cell is treasure or not
+	private boolean isFlaged;			// The cell is flag
+	private boolean isDoubt;			// The cell is marked as doubt
+	
+	public Cell(Context context) {
+		super(context);
+	}
 
 }
