dc155bf107340f4b044d35c8bb2f4bad7cd2955e
Cường Phạm <cuongphambk2010@gmail.com>
2014-01-07 15:59:06 +0700

    8B: Add basic layout for game screen

--- gen/com/example/treasurehunt/R.java
+++ gen/com/example/treasurehunt/R.java
@@ -38,11 +38,14 @@ public final class R {
         public static final int treasurehunt=0x7f02000e;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int loadGameBtn=0x7f080002;
-        public static final int newGameBtn=0x7f080003;
-        public static final int rankingBtn=0x7f080000;
-        public static final int recordBtn=0x7f080001;
+        public static final int action_settings=0x7f080007;
+        public static final int levelText=0x7f080000;
+        public static final int livesText=0x7f080002;
+        public static final int loadGameBtn=0x7f080005;
+        public static final int newGameBtn=0x7f080006;
+        public static final int rankingBtn=0x7f080003;
+        public static final int recordBtn=0x7f080004;
+        public static final int timeText=0x7f080001;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
